afb1e5b558be0406de87058905d3d2ed8aece750
datadragon99 <166937959+datadragon99@users.noreply.github.com>
2024-04-24 18:45:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import tensorflow as tf
 import numpy as np
 from tensorflow.keras.preprocessing.image import array_to_img
+import base64
+from io import BytesIO
 
 # Load the trained generator model
 generator = tf.keras.models.load_model('generator.h5')
@@ -16,15 +18,27 @@ def generate_images(num_images=1, randomness_level=1.0):
     noise = tf.random.normal([num_images, latent_dim]) * randomness_level
     generated_images = generator(noise, training=False)
     generated_images = (generated_images * 127.5) + 127.5  # Denormalize the images
+    return generated_images
 
-    for i in range(num_images):
-        img = array_to_img(generated_images[i])
-        st.image(img, caption=f'Generated Image {i+1}', use_column_width=True)
+# Function to download an image
+def download_image(image, file_name):
+    img = array_to_img(image)
+    buf = BytesIO()
+    img.save(buf, format="PNG")
+    byte_im = buf.getvalue()
+    base64_str = base64.b64encode(byte_im).decode('utf-8')
+    href = f'<a href="data:image/png;base64,{base64_str}" download="{file_name}.png">Download {file_name}.png</a>'
+    return href
 
 # Sidebar for user input
 st.sidebar.title('Options')
-num_images = st.sidebar.slider('Number of Images', 1, 10, 1, 1)
+num_images = st.sidebar.slider('Number of Images', 1, 10, 5, 1)
 randomness_level = st.sidebar.slider('Randomness Level', 0.0, 2.0, 1.0, 0.1)
 
 if st.sidebar.button('Generate Images'):
-    generate_images(num_images, randomness_level)
+    generated_images = generate_images(num_images, randomness_level)
+
+    cols = st.columns(num_images)
+    for i, col in enumerate(cols):
+        col.image(generated_images[i], caption=f'Generated Image {i+1}', use_column_width=True)
+        col.markdown(download_image(generated_images[i], f'image_{i+1}'), unsafe_allow_html=True)
