6b45f94342e747009686dc175d36dce3046df52b
datadragon99 <166937959+datadragon99@users.noreply.github.com>
2024-04-24 18:54:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import tensorflow as tf
 import numpy as np
 from tensorflow.keras.preprocessing.image import array_to_img
-from zipfile import ZipFile
+import base64
 from io import BytesIO
 
 # Load the trained generator model
@@ -20,25 +20,15 @@ def generate_images(num_images=1, randomness_level=1.0):
     generated_images = (generated_images * 127.5) + 127.5  # Denormalize the images
     return generated_images
 
-# Function to download images as a ZIP file
-def download_images(generated_images):
-    zip_buffer = BytesIO()
-    with ZipFile(zip_buffer, 'w') as zip_file:
-        for i, img in enumerate(generated_images):
-            img_data = array_to_img(img)
-            file_name = f'image_{i+1}.png'
-            img_buffer = BytesIO()
-            img_data.save(img_buffer, format='PNG')
-            img_buffer.seek(0)
-            zip_file.writestr(file_name, img_buffer.getvalue())
-
-    zip_buffer.seek(0)
-    st.download_button(
-        label="Download Images",
-        data=zip_buffer,
-        file_name="generated_images.zip",
-        mime="application/zip",
-    )
+# Function to download an image
+def download_image(image, file_name):
+    img = array_to_img(image)
+    buf = BytesIO()
+    img.save(buf, format="PNG")
+    byte_im = buf.getvalue()
+    base64_str = base64.b64encode(byte_im).decode('utf-8')
+    href = f'<a href="data:image/png;base64,{base64_str}" download="{file_name}.png">Download {file_name}.png</a>'
+    return href
 
 # Sidebar for user input
 st.sidebar.title('Options')
@@ -47,8 +37,7 @@ randomness_level = st.sidebar.slider('Randomness Level', 0.0, 2.0, 1.0, 0.1)
 
 if st.sidebar.button('Generate Images'):
     generated_images = generate_images(num_images, randomness_level)
-    for i, img in enumerate(generated_images):
-        img_data = array_to_img(img)
-        st.image(img_data, caption=f'Generated Image {i+1}', use_column_width=True)
 
-    download_images(generated_images)
+    for i, img in enumerate(generated_images):
+        st.image(img, caption=f'Generated Image {i+1}', use_column_width=True)
+        st.markdown(download_image(img, f'image_{i+1}'), unsafe_allow_html=True)
